9dfeb3ab07b93919be81f731dabfa168e35b65f4
duda4418 <146849849+duda4418@users.noreply.github.com>
2024-07-03 21:02:17 +0300

    Implementing winning and losing logic

--- wordle_wannabe/src/app/game/game.tsx
+++ wordle_wannabe/src/app/game/game.tsx
@@ -1,126 +1,159 @@
 'use client'
 import React, { useEffect, useState } from 'react';
-import LetterBox from './boxLetter'
+import LetterBox from './boxLetter';
 
-const WordleGame = ({letterList}:any) => {
-    
+const WordleGame = ({ letterList }: any) => {
     const searchedWordList = ['C', 'A', 'A', 'O', 'E'];
     const [input, setInput] = useState<string>('');
-    
+    const [winning, setWinning] = useState(false);
+    const [modalOpen, setModalOpen] = useState(false)
+
     useEffect(() => {
         const handleKeyDown = (event: KeyboardEvent) => {
             const { key } = event;
-            //Handle enter key
+
             if (key === 'Enter') {
-                if (input.length == 5){
+                if (input.length === 5) {
                     let letters: string[] = input.split('');
                     const auxList = [...searchedWordList];
                     const auxLetters = [...letters];
-                    for(let i = 0; i < 30; i++){
-                        if(letterList[i].letter === '' || letterList[i].letter === undefined){
-                            letterList[i-5].letter = letters[0];
-                            if(auxList[i%5] == letters[0]){
-                                letterList[i-5].color = 'green';
-                                auxList[i%5] = '-'
-                                auxLetters[i%5] = '-'
+                    for (let i = 0; i < 30; i++) {
+                        if (letterList[i].letter === '' || letterList[i].letter === undefined) {
+                            letterList[i - 5].letter = letters[0];
+                            if (auxList[i % 5] === letters[0]) {
+                                letterList[i - 5].color = 'green';
+                                auxList[i % 5] = '-';
+                                auxLetters[i % 5] = '-';
                             }
                             letters.shift();
-                        }else if(i>24){
+                        } else if (i > 24) {
                             letterList[i].letter = letters[0];
-                            if(auxList[i%5] == letters[0]){
+                            if (auxList[i % 5] === letters[0]) {
                                 letterList[i].color = 'green';
-                                auxList[i%5] = '-'
-                                auxLetters[i%5] = '-'
+                                auxList[i % 5] = '-';
+                                auxLetters[i % 5] = '-';
                             }
                             letters.shift();
                         }
                     }
-                    
-                    for(let i = 0; i < 30; i++){
-                        if(letterList[i].letter === '' || letterList[i].letter === undefined){
-                            if(auxLetters[0] != '-'){
-                                if (auxList.includes(auxLetters[0])){
-                                    letterList[i-5].letter = auxLetters[0]; 
-                                    letterList[i-5].color = 'yellow';
-                                    const index = auxList.indexOf(auxLetters[0])
-                                    auxList[index] = '-'
-                                }
-                                else{
-                                    letterList[i-5].color = 'gray';
+
+                    for (let i = 0; i < 30; i++) {
+                        if (letterList[i].letter === '' || letterList[i].letter === undefined) {
+                            if (auxLetters[0] !== '-') {
+                                if (auxList.includes(auxLetters[0])) {
+                                    letterList[i - 5].letter = auxLetters[0];
+                                    letterList[i - 5].color = 'yellow';
+                                    const index = auxList.indexOf(auxLetters[0]);
+                                    auxList[index] = '-';
+                                } else {
+                                    letterList[i - 5].color = 'gray';
                                 }
                             }
                             auxLetters.shift();
-                        }
-                        else if(i>24){
-                            if(auxLetters[0] != '-'){
-                                if (auxList.includes(auxLetters[0])){
-                                    letterList[i].letter = auxLetters[0]; 
+                        } else if (i > 24) {
+                            if (auxLetters[0] !== '-') {
+                                if (auxList.includes(auxLetters[0])) {
+                                    letterList[i].letter = auxLetters[0];
                                     letterList[i].color = 'yellow';
-                                    const index = auxList.indexOf(auxLetters[0])
-                                    auxList[index] = '-'
-                                }
-                                else{
+                                    const index = auxList.indexOf(auxLetters[0]);
+                                    auxList[index] = '-';
+                                } else {
                                     letterList[i].color = 'gray';
                                 }
                             }
                             auxLetters.shift();
                         }
                     }
+                    if (letterList.slice(25, 30).every((item: { color: string; }) => item.color === 'green')) {
+                        setWinning(true);
+                        setModalOpen(true);
+                    }else if(letterList.slice(25, 30).every((item: { color: string; }) => item.color != '')){
+                        setWinning(false);
+                        setModalOpen(true)
+                    }
                     setInput('');
                 }
-        } else if (key === 'Backspace') {
-          // Handle Backspace key press
-          if(input){
-            for(let i = 0; i < 30; i++){
-                if(letterList[i].letter === '' || letterList[i].letter === undefined){
-                    letterList[i-1].letter = '';
-                    letterList[i-1].color = 'gray';
-                    break;
+            } else if (key === 'Backspace') {
+                if (input) {
+                    for (let i = 0; i < 30; i++) {
+                        if (letterList[i].letter === '' || letterList[i].letter === undefined) {
+                            letterList[i - 1].letter = '';
+                            letterList[i - 1].color = 'gray';
+                            break;
+                        }
+                    }
+                }
+                setInput((prevInput) => prevInput.slice(0, -1));
+            } else {
+                if (input.length < 5) {
+                    const uppercaseKey = key.toUpperCase();
+                    if (/^[A-Z]$/.test(uppercaseKey)) {
+                        for (let i = 0; i < 30; i++) {
+                            if (letterList[i].letter === '' || letterList[i].letter === undefined) {
+                                letterList[i].letter = uppercaseKey;
+                                letterList[i].color = 'outline';
+                                break;
+                            }
+                        }
+                        setInput((prevInput) => prevInput + uppercaseKey);
+                    }
                 }
             }
-        }
-          setInput((prevInput) => prevInput.slice(0, -1));
-        } else {
-          // Handle other key presses
-          if (input.length < 5) {
-            const uppercaseKey = key.toLocaleUpperCase(); 
-            for(let i = 0; i < 30; i++){
-                if(letterList[i].letter === '' || letterList[i].letter === undefined){
-                    letterList[i].letter = uppercaseKey;
-                    letterList[i].color = 'outline';
-                    break;
-                }
-            }
-            setInput((prevInput) => prevInput + uppercaseKey);
-        }
-        }
-      };
-  
-      window.addEventListener('keydown', handleKeyDown);
-  
-      // Cleanup event listener on component unmount
-      return () => {
-        window.removeEventListener('keydown', handleKeyDown);
-      };
-    }, [input]); 
+        };
+        
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [input]);
+
     return (
         <div>
+            {modalOpen && winning &&(
+                <>
+                    <dialog id="my_modal_4" className="modal modal-open">
+                        <div className="modal-box">
+                            <form method="dialog">
+                                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" onClick={()=>setModalOpen(false)}>✕</button>
+                            </form>
+                            <h3 className="font-bold text-lg">YOU WON!</h3>
+                            <p className="py-4">Press ESC key or click on ✕ button to close</p>
+                        </div>
+                    </dialog>
+                </>
+            )}
+            {modalOpen && !winning &&(
+                <>
+                    <dialog id="my_modal_4" className="modal modal-open">
+                        <div className="modal-box">
+                            <form method="dialog">
+                                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" onClick={()=>setModalOpen(false)}>✕</button>
+                            </form>
+                            <h3 className="font-bold text-lg">YOU LOST!</h3>
+                            <p className="py-4">Press ESC key or click on ✕ button to close</p>
+                        </div>
+                    </dialog>
+                </>
+            )}
             <p>{input}</p>
             <p>{input.length}</p>
             <div className='flex flex-col items-center justify-center'>
                 {[...Array(6)].map((_, i) => (
-                    <div className='flex flex-row'>
+                    <div className='flex flex-row' key={i}>
                         {[...Array(5)].map((_, index) => (
-                            <div className='m-1'>
-                                <LetterBox letter={letterList[index+i*5] ? letterList[index+i*5].letter : ''}
-                                    color={letterList[index+i*5] ? letterList[index+i*5].color : ''} />
+                            <div className='m-1' key={index}>
+                                <LetterBox
+                                    letter={letterList[index + i * 5] ? letterList[index + i * 5].letter : ''}
+                                    color={letterList[index + i * 5] ? letterList[index + i * 5].color : ''}
+                                />
                             </div>
                         ))}
                     </div>
                 ))}
             </div>
         </div>
-    )
+    );
 }
 
-export default WordleGame
+export default WordleGame;
